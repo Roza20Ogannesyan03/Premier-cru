69ce0b4a2cff85214948908ac68722e2e7915254
Roza <rozaogannesyan109@gmail.com>
2023-07-24 17:55:24 +0300

    версия предпоследняя

--- about-us.js
+++ about-us.js
@@ -1,13 +1,11 @@
 var swiper = new Swiper(".about-us__swiper", {
-    simulateTouch: true,
-    navigation: {
-      nextEl: ".about-us__swiper-button-next",
-      prevEl: ".about-us__swiper-button-prev",
-    },
-   
-  
- 
-    slidesPerView: 4,
-    loop: true,
-    spaceBetween: 44,
-  });
+  navigation: {
+    nextEl: ".about-us__swiper-button-next",
+    prevEl: ".about-us__swiper-button-prev",
+  },
+
+  slidesPerView: 4,
+  slidesPerGroup: 1,
+  loop: true,
+  spaceBetween: 44,
+});
--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 var swiper = new Swiper(".swiper", {
-    // simulateTouch: true,
-     navigation: {
-       nextEl: ".slider__swiper-button-next",
-       prevEl: ".slider__swiper-button-prev",
-     },
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    loop: true,
-    spaceBetween: 60,
-    // on: {
-    //   slideChange: function (swiper) {
-    //     console.log("bbb");
-    //     document.querySelector(".first-slide__news").innerHTML =
-    //       (swiper.realIndex < 10 ? "0" : "") + (+swiper.realIndex + 1);
-    //   },
-    // },
-  
-    // Keyboard: {
-    //   enabled: true,
-    //   onlyInViueport: true,
-    //   pageUpDown: true,
-    // },
-    // autoHeight: false,
-    // //slidesPerView: 4,
-    // slidesPerGroup: 1,
-    // //
-    // //loop: true,
-  });
+  navigation: {
+    nextEl: ".slider__swiper-button-next",
+    prevEl: ".slider__swiper-button-prev",
+  },
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 60,
+  on: {
+    slideChange: function (swiper) {
+      console.log(swiper);
+      document.querySelector(".first-slide__news").innerHTML =
+        (swiper.realIndex < 10 ? "0" : "") + (+swiper.realIndex + 1);
+    },
+  },
+
+  // Keyboard: {
+  //   enabled: true,
+  //   onlyInViueport: true,
+  //   pageUpDown: true,
+  // },
+  // autoHeight: false,
+  // //slidesPerView: 4,
+  // slidesPerGroup: 1,
+  // //
+  // //loop: true,
+});
