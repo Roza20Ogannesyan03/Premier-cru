477448e7a4d3a9afa39740d5f8f26f60dcb4c17f
Roza <rozaogannesyan109@gmail.com>
2023-07-25 18:03:41 +0300

    работающие слайдеры

--- about-us.js
+++ about-us.js
@@ -1,11 +1,18 @@
-var swiper = new Swiper(".about-us__swiper", {
+const swiper1 = new Swiper(document.getElementById("kupol"), {
   navigation: {
     nextEl: ".about-us__swiper-button-next",
     prevEl: ".about-us__swiper-button-prev",
   },
 
-  slidesPerView: 4,
+  slidesPerView: "auto",
   slidesPerGroup: 1,
   loop: true,
   spaceBetween: 44,
+});
+
+const activeSlide = document.querySelectorAll(".about-us__slide");
+activeSlide.forEach((slide) => {
+  slide.addEventListener("click", function () {
+    slide.classList.toggle("active");
+  });
 });
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var swiper = new Swiper(".swiper", {
+//let paginat = document.querySelector(".swiper-pagination");
+const swiper = new Swiper(document.getElementById("rubl"), {
   navigation: {
     nextEl: ".slider__swiper-button-next",
     prevEl: ".slider__swiper-button-prev",
@@ -7,14 +8,37 @@ var swiper = new Swiper(".swiper", {
   slidesPerView: "auto",
   loop: true,
   spaceBetween: 60,
-  on: {
+  /*on: {
     slideChange: function (swiper) {
       console.log(swiper);
       document.querySelector(".first-slide__news").innerHTML =
         (swiper.realIndex < 10 ? "0" : "") + (+swiper.realIndex + 1);
     },
-  },
+  },*/
 
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    formatFractionCurrent: function (number, index) {
+      return ("0" + number).slice(-2);
+    },
+    formatFractionTotal: function (number, tot) {
+      return ("0" + number).slice(-2);
+    },
+    renderFraction: function (currentClass, totalClass) {
+      //console.log(paginat.innerHTML);
+      console.log(currentClass);
+      return (
+        '<span class = "' +
+        currentClass +
+        ' "></span>' +
+        "/" +
+        '<span class = "' +
+        totalClass +
+        ' "></span>'
+      );
+    },
+  },
   // Keyboard: {
   //   enabled: true,
   //   onlyInViueport: true,
